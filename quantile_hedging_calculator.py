a99d02c9ec9da23b939b1aa805dd3a0d97265677
michalsn13 <michaldabrowski1998@gmail.com>
2023-03-23 07:48:09 +0100

    Quantile Hedging implemented, some things to discuss still

---
+++ quantile_hedging_calculator.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import numpy as np
+
+def payoff_from_v0(option, init_capital, X0, n_sims = 10000, MC_method = 'crude'):
+    
+    WX = option.underlying.simulate_P(n_sims, T=option.T)
+    order = WX[1][option.underlying.values_per_year * option.T].argsort()
+    W_sorted = WX[0].iloc[order, :]
+    X_sorted = WX[1].iloc[order, :]
+
+    BS_Price = option.get_MC_price(X0, 0, n_sims, MC_method)
+    if BS_Price <= init_capital:
+        raise Exception('With that initial capital you can perform full hedging')
+
+    dP_dQstar = (np.exp(
+        (option.underlying.mu - option.underlying.r) * W_sorted[option.underlying.values_per_year * option.T] / option.underlying.sigma +
+        (0.5 * ((option.underlying.mu - option.underlying.r) / option.underlying.sigma) ** 2 +
+         option.underlying.r) * option.T) * BS_Price / option.payoff_func(X0*X_sorted))
+
+    dQstar_dP = 1 / dP_dQstar
+
+    hedge_prob = init_capital / BS_Price
+    index = sum(np.cumsum(dQstar_dP) / n_sims < hedge_prob)
+    success_prob = index/n_sims
+    if index == -1:
+        raise Exception('You cannot perform any hedging with so little initial capital')
+
+    c = X0*X_sorted.iloc[index - 1, -1]
+    old_payoff = option.payoff_func
+
+    def payoff_1A(X):
+        return old_payoff(X)*(X.iloc[:,-1] <= c)
+    return (payoff_1A, success_prob)
+
+def payoff_from_prob(option, success_prob, X0, n_sims = 10000, MC_method = 'crude'):
+    if 1 <= success_prob:
+        raise Exception('To hedge with 100% certainty use full hedge')
+
+    WX = option.underlying.simulate_P(n_sims, T=option.T)
+    order = WX[1][option.underlying.values_per_year * option.T].argsort()
+    W_sorted = WX[0].iloc[order, :]
+    X_sorted = WX[1].iloc[order, :]
+
+    BS_Price = option.get_MC_price(X0, 0, n_sims, MC_method)
+
+    dP_dQstar = (np.exp(
+        (option.underlying.mu - option.underlying.r) * W_sorted[option.underlying.values_per_year * option.T] / option.underlying.sigma +
+        (0.5 * ((option.underlying.mu - option.underlying.r) / option.underlying.sigma) ** 2 +
+         option.underlying.r) * option.T) * BS_Price / option.payoff_func(X0*X_sorted))
+
+    dQstar_dP = 1 / dP_dQstar
+
+    index = int(success_prob * n_sims)
+    init_capital = dQstar_dP[:index].sum()/n_sims * BS_Price
+
+    c = X0*X_sorted.iloc[index - 1, -1]
+
+    def payoff_1A(X):
+        return option.payoff_func(X) * (X.iloc[:, -1] <= c)
+
+    return (payoff_1A, init_capital)
--- trader.py
+++ trader.py
@@ -1,16 +1,25 @@
 import numpy as np
 from scipy.stats import norm
+from quantile_hedging_calculator import *
 
 class Trader:
-    def __init__(self, money, delta = 0):
-        self.money = money
-        self.delta = delta
+    def __init__(self, initial_capital, delta = 0):
+        self.money = initial_capital
+        self.delta = 0
     def update_portfolio(self, option, underlying_price, t):
         delta_curr = option.get_MC_delta(underlying_price, t)
         #delta_curr = norm.cdf((np.log(underlying_price/80) + (option.underlying.r + 0.5 * option.underlying.sigma **2) * (option.T - t))/np.sqrt(option.underlying.sigma **2 * (option.T - t)))
         self.money = self.money - (delta_curr - self.delta) * underlying_price 
         self.delta = delta_curr
-    def full_hedge(self, option, reality, update_freq = 1, verbose = False):
+    def simulate_hedging(self, option, reality, update_freq = 1, limited_capital = False, verbose = False):
+        if limited_capital:
+            new_payoff_func,success_prob = payoff_from_v0(option, self.money, float(reality[0]))
+            if verbose:
+                print(f'Quantile Hedging with V0={self.money:.2f} should result in p = {success_prob:.4} probability of success')
+            old_payoff_func = option.payoff_func
+            setattr(option, 'payoff_func', new_payoff_func)
+        else:
+            success_prob = 1
         values_per_expirance = option.underlying.values_per_year * option.T
         money_historical = []
         delta_historical = []
@@ -36,4 +45,8 @@ class Trader:
         money_historical.append(self.money)
         if verbose:
             print(f'Payoff of {payoff:.2f} paid to the option owner! Current status\n\tMONEY: {self.money:.2f}\n\tUNDERLYING: {self.delta:.4f}')
-        return money_historical, delta_historical
+            
+        if limited_capital:
+            setattr(option, 'payoff_func', old_payoff_func)
+        
+        return money_historical, delta_historical, success_prob
